61f0af258735a1639cab65cfa9ef2498be28e7c2
jorginhoMachado <jorge.vmachado@hotmail.com>
2020-01-14 22:15:12 -0300

    FUCK

--- src/main/java/br/com/jorgevmachado/springjava/services/PedidoService.java
+++ src/main/java/br/com/jorgevmachado/springjava/services/PedidoService.java
@@ -64,4 +64,31 @@ public class PedidoService {
         return obj;
     }
 
+//    {
+//        "cliente": {
+//        "id": 1
+//    },
+//        "enderecoDeEntrega": {
+//        "id": 1
+//    },
+//        "pagamento": {
+//        "numeroDeParcelas": 10,
+//                "@type": "pagamentoComCartao"
+//    },
+//        "itens": [
+//        {
+//            "quantidade": 2,
+//                "produto": {
+//            "id": 3
+//        }
+//        },
+//        {
+//            "quantidade": 1,
+//                "produto" : {
+//            "id": 1
+//        }
+//        }
+//	]
+//    }
+
 }
